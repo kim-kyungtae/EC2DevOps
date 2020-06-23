7ce1a5b811f63197d03eee00b316e97e8c6473ee
kim-kyungtae <62549721+kim-kyungtae@users.noreply.github.com>
2020-06-23 14:21:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love kkt01');
+    res.send('I love kkt02');
 });
 
 app.get('/start', function(req, res){
